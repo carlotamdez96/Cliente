e489da67bb0d777038bcd7b26d8a8b61c41d107d
carlotmdez96 <carlotamenendez96@hotmail.com>
2022-11-28 14:24:59 +0100

    hola

--- Nueva carpeta/Preguntados/script/funcionesIndex.js
+++
@@ -1,6 +0,0 @@
-import{cuestionario} from "./preguntas.js";
-import{getImagen} from "./imagenesHTML.js";
-
-const parteImagenes = document.querySelector(".imagenesMaterias");
-parteImagenes.innerHTML+=getImagen();
-
